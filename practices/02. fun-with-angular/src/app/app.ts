4ca75c84bf893059a9e0cc8c55924ce3a9d55d4d
Kobi Hari <kobi2294@yahoo.com>
2026-09-04 15:35:23 +0300

    S03L05 - Angular is Dynamic

--- practices/02. fun-with-angular/src/app/app.ts
+++ practices/02. fun-with-angular/src/app/app.ts
@@ -8,4 +8,17 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('fun-with-angular');
+
+  constructor() {
+    console.log('Starting to run');
+    setTimeout(() => {
+      this.title.set('Hello World');
+      console.log('Changing the title');
+    }, 5000);
+
+  }
 }
+
+// 1. Creates an instance of the component class
+// 2. Adds a clone of the template to the Host element
+// 3. Adds Bindings to the template
